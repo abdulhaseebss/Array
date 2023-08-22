bb3726eb5eff2225677ba6b69613e2668aa86d80
abdulhaseebss <m.saleemhaseeb@gmail.com>
2023-08-22 22:56:18 +0500

    Array Assignment

---
+++ app.js
@@ -0,0 +1,50 @@
+// Q=1
+
+// const studentNames = ["Alice", "Bob", "Charlie", "David", "Eve"];
+// console.log(studentNames);
+
+
+// Q=2
+
+// const studentNames = ["Alice", "Bob", "Charlie", "David", "Eve"];
+// const insertionIndex = 2;
+// studentNames.splice(insertionIndex + 1, 0, "Frank");
+// console.log(studentNames);
+
+// Q=3
+
+// const studentNames = ["Alice", "Bob", "Charlie", "Frank", "David", "Eve"];
+// const nameToRemove = "David";
+
+// const indexToRemove = studentNames.indexOf(nameToRemove);
+
+// if (indexToRemove !== -1) {
+//   studentNames.splice(indexToRemove, 1);
+// }
+
+// console.log(studentNames);
+
+// Q=4
+
+// const studentNames = ["Alice", "Bob", "Charlie", "Frank", "Eve"];
+// studentNames.push("Grace", "Helen");
+
+// console.log(studentNames);
+
+// Q=5
+
+// const studentNames = ["Alice", "Bob", "Charlie", "Frank", "Eve"];
+// const indexToReplace = studentNames.indexOf("Bob");
+
+// if (indexToReplace !== -1) {
+//   studentNames[indexToReplace] = "John";
+// }
+
+// console.log(studentNames);
+
+// Q=6
+
+// const studentNames = ["Alice", "John", "Charlie", "Frank", "Eve", "Grace", "Helen"];
+// console.log(studentNames);
+
+
